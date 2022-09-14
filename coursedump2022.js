9bfefa0acad9157a7086ce21ffc92c62557623fb
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2022-09-14 12:15:46 +0200

    Fix stopping after an empty set

--- coursedump2022.js
+++ coursedump2022.js
@@ -1,34 +1,40 @@
 url = window.location.toString();
 course = url.split("/");
-if (course[3] === "course") {id = course[4];} else {alert("Please go to a Memrise course page first");throw '';};
+if (course[3] === "course") { id = course[4]; } else { alert("Please go to a Memrise course page first"); throw ''; };
 
 next = true;
 
 //based on the script by baroxyton
 //https://gist.github.com/baroxyton/01a53533ff49a62b5a94dd8b26f38d31
 result = "";
-(async function(){
-for (let i = 1; next; i++) {
-    try {
-	    // get CSRF header
-	    token = document.cookie.split(" ").find(cookie => cookie.includes("csrftoken")).split(/[=;]/g)[1];
-	    response = await(await fetch("https://app.memrise.com/v1.18/learning_sessions/preview/", {
-	        "headers": { "Accept": "*/*", "Content-Type": "Application/json", "X-CSRFToken": token },
-	        "body": "{\"session_source_id\":"+id+",\"session_source_type\":\"course_id_and_level_index\",\"session_source_sub_index\":" + i + "}",
- 	        "method": "POST"
-	    })).json();
-	    // Escape double quotes and commas
-	    result += response.learnables.map(learnable => `"${learnable.learning_element.replace('"', '""')}","${learnable.definition_element.replace('"', '""')}"`).join("\n")+"\n"
-	} catch (error) {
-		next = false;
+(async function () {
+	for (let i = 1; next; i++) {
+		let empty_set_err = false;
+		try {
+			// get CSRF header
+			token = document.cookie.split(" ").find(cookie => cookie.includes("csrftoken")).split(/[=;]/g)[1];
+			response = await (await fetch("https://app.memrise.com/v1.18/learning_sessions/preview/", {
+				"headers": { "Accept": "*/*", "Content-Type": "Application/json", "X-CSRFToken": token },
+				"body": "{\"session_source_id\":" + id + ",\"session_source_type\":\"course_id_and_level_index\",\"session_source_sub_index\":" + i + "}",
+				"method": "POST"
+			})).json();
+			// Continue after empty set
+			if (response.code == "PREVIEW_DOESNT_LOAD") {
+				empty_set_err = true;
+			}
+			// Escape double quotes and commas
+			result += response.learnables.map(learnable => `"${learnable.learning_element.replace('"', '""')}","${learnable.definition_element.replace('"', '""')}"`).join("\n") + "\n"
+		} catch (error) {
+			if (empty_set_err) continue;
+			next = false;
+		}
 	}
-}
-  
-var hiddenElement = document.createElement('a');  
-hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(result);  
-hiddenElement.target = '_blank';  
-      
-hiddenElement.download = course[5]+'.csv';  
-hiddenElement.click();  
+
+	var hiddenElement = document.createElement('a');
+	hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(result);
+	hiddenElement.target = '_blank';
+
+	hiddenElement.download = course[5] + '.csv';
+	hiddenElement.click();
 
 })()
