8b394d6341c9710690ce475a8615260e4e3d4484
Eltaurus-Lt <93875472+Eltaurus-Lt@users.noreply.github.com>
2022-10-17 04:45:17 +0300

    Multiple media support
    - support for multiple audio/video files in one learnable item
    - separate columns for audio and video
    - fix for learnable items with non-null audio of 0 length
    - fix for different numbers of cells per row
    - general refactoring

--- coursedump2022.js
+++ coursedump2022.js
@@ -1,40 +1,28 @@
-let media_asked = false;
-let download_media = false;
-let anki_media = false;
-let media_urls = [];
-let url = window.location.toString();
-let id;
+
 const MAX_ERR_ABORT = 5;
-course = url.split("/");
-function anki_media_help() {
-	let OS = "Unknown";
-	if (navigator.userAgent.indexOf("Win") != -1) OS = "Windows";
-	if (navigator.userAgent.indexOf("Mac") != -1) OS = "MacOS";
-	if (navigator.userAgent.indexOf("Linux") != -1) OS = "Linux";
-	let locations = {
-		"Linux": "On Linux, it is found at ~/.local/share/Anki2/[username]/collection.media for native installs or at ~/.var/app/net.ankiweb.Anki/data/Anki2/[username]/collection.media for flatpak installs",
-		"Unknown": "Location unknown for " + navigator.platform,
-		"MacOS": "On Mac, it is found at ~/Library/Application Support/Anki2/[username]/collaction.media. The Library folder is hidden by default, but can be revealed in Finder by holding down the option key while clicking on the Go menu",
-		"Windows": "On Windows, it is found at %APPDATA%\\Anki2\\[username]\\collection.media"
 
-	}
-	let helpMessage = `To properly intergrate media with Anki, you need to copy the newly created media folder in your downloads folder to your anki media folder.
 
-${locations[OS]}`
-	alert(helpMessage);
-}
+course = window.location.toString().split("/");
+if (course[3] === "course") { 
+	id = course[4]; 
+	name = course[5];
+} else { alert("Please use the extention on an open Memrise course tab"); throw ''; };
+
+
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
-if (course[3] === "course") { id = course[4]; } else { alert("Please go to a Memrise course page first"); throw ''; };
 
-next = true;
-
-//based on the script by baroxyton
-//https://gist.github.com/baroxyton/01a53533ff49a62b5a94dd8b26f38d31
-result = "";
 (async function () {
 	let err_count = 0;
+	let media_asked = false;
+	let download_media = false;
+	let has_audio = false;
+	let has_video = false;
+	let media_download_urls = [];
+	let table = [];
+
+	let next = true;
 	for (let i = 1; next; i++) {
 		let empty_set_err = false;
 		try {
@@ -51,41 +39,41 @@ result = "";
 				empty_set_err = true;
 			}
 			// Check for media
-			if (!media_asked && response.learnables.find(learnable => { return (learnable.screens["1"].audio || learnable.screens["1"].video) })) {
+			if (!media_asked && response.learnables.find(learnable => { return ( (learnable.screens["1"].audio && learnable.screens["1"].audio.value.length > 0) || (learnable.screens["1"].video && learnable.screens["1"].video.value.length > 0) ) })) {
 				media_asked = true;
-
-				const enable_downloads = confirm("Embedded media was detected. Would you like to download it?");
-				if (enable_downloads) {
-					download_media = true;
-					if (confirm("Would you like to intergrate the media with Anki?")) {
-						anki_media = true;
-					}
-				}
+				download_media = confirm("Embedded media was detected. Would you like to download it?");
 			}
-			// Escape double quotes and commas
-			result += response.learnables.map(learnable => {
-				let mediaUrl;
-				if (download_media) {
-					if (learnable.screens["1"].audio) {
-						mediaUrl = learnable.screens["1"].audio.value[0].normal;
-					}
-					if (learnable.screens["1"].video) {
-						mediaUrl = learnable.screens["1"].video.value[0];
-					}
+
+
+			// Creating the table and queueing media files
+			response.learnables.map(learnable => {
+
+				let row = [`"${learnable.learning_element.replace('"', '""')}"`,`"${learnable.definition_element.replace('"', '""')}"`];
+
+				//audio
+				let temp_audio_urls = [];
+				if (download_media && learnable.screens["1"].audio && learnable.screens["1"].audio.value.length > 0) {
+					has_audio = true;
+					learnable.screens["1"].audio.value.map(audio_item => {temp_audio_urls.push(audio_item.normal)});
+					media_download_urls.push(...temp_audio_urls);
 				}
-				if (mediaUrl) {
-					media_urls.push(mediaUrl);
+				row.push(`"` + temp_audio_urls.map(url => `[sound:${name}_media/${url.split("/").slice(-1)}]`).join("") + `"`);
+
+				//video
+				let temp_video_urls = [];
+				if (download_media && learnable.screens["1"].video && learnable.screens["1"].video.value.length > 0) {
+					has_video = true;
+					learnable.screens["1"].video.value.map(video_item => {temp_video_urls.push(video_item)});
+					media_download_urls.push(...temp_video_urls);
 				}
-				if (anki_media && mediaUrl) {
-					let filename = `${id}_media/${mediaUrl.split("/").slice(-1)}`;
-					return `"${learnable.learning_element.replace('"', '""')} [sound:${filename}]","${learnable.definition_element.replace('"', '""')}"`
-				}
-				return `"${learnable.learning_element.replace('"', '""')}","${learnable.definition_element.replace('"', '""')}"`
-			}).join("\n") + "\n";
+				row.push(`"` + temp_video_urls.map(url => `[sound:${name}_media/${url.split("/").slice(-1)}]`).join("") + `"`);
+					
+				table.push(row);
+			});
 
 			err_count = 0;
 		} catch (error) {
-			console.log(error)
+			console.log(error);
 			console.log(err_count);
 			if (empty_set_err) continue;
 			err_count++;
@@ -94,23 +82,37 @@ result = "";
 			}
 		}
 	}
-	console.log(media_urls)
 
+
+	//table to text conversion (global flags has_audio/has_video are needed since different number of cells in csv rows causes problems for Anki import)
+	let result = table.map(row => {
+		let line = row[0] + `,` + row[1];
+		if (has_audio) {line += `,` + row[2]}
+		if (has_video) {line += `,` + row[3]}
+		return line;
+	}).join("\n") + "\n";
+
+
+	//downloading the table
 	var hiddenElement = document.createElement('a');
 	hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(result);
 	hiddenElement.target = '_blank';
-
-	hiddenElement.download = course[5] + '.csv';
+	hiddenElement.download = name + '.csv';
 	hiddenElement.click();
 
+	//help
+	if (confirm('Would you like some help with Anki integration?')) {
+		window.open('https://github.com/Eltaurus-Lt/CourseDump2022#importing-in-anki', '_blank').focus();
+	};
+
+	//downloading audio and video files
 	if (download_media) {
+
 		var param = {
-			collection: media_urls,
-			folder: `${id}_media`
+			collection: media_download_urls,
+			folder: `${name}_media`
 		};
 		chrome.runtime.sendMessage(param);
-		if (anki_media) {
-			anki_media_help()
-		}
 	}
+
 })();
