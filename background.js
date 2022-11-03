da234c2a72c8ddfa8eaf142d18ad5d491cc5007a
Eltaurus-Lt <93875472+Eltaurus-Lt@users.noreply.github.com>
2022-11-04 04:24:19 +0600

    v6.0 Graphical progress indicator and Metadata
    also
    - added types to messages sent between background and main scripts
    - fixed character escaping issue in tags

--- background.js
+++ background.js
@@ -11,18 +11,34 @@ function sleep(ms) {
 
 chrome.runtime.onMessage.addListener(
    function (arg, sender, sendResponse) {
+      var type = arg.type;
       var mediaurls = arg.collection;
-      var subfolder = arg.folder;
-      (async function () {
-         for (i in mediaurls) {
-            await sleep(200);
-            var mediaUrl = mediaurls[i][0];
-            var saveas = mediaurls[i][1];
 
-            chrome.downloads.download({
-               url: mediaUrl,
-               filename: saveas
-            });
-         }
-      })()
+	if (type === "coursedump_download") {
+	      (async function () {
+	         for (i in mediaurls) {
+	            await sleep(200);
+	            var mediaUrl = mediaurls[i][0];
+	            var saveas = mediaurls[i][1];
+
+	            chrome.downloads.download({
+	               url: mediaUrl,
+	               filename: saveas
+	            });
+
+		    chrome.tabs.sendMessage(sender.tab.id, {
+		        type: "coursedump_progress_upd",
+		        progress: "" + Math.round((10000 + 10000. * i) / mediaurls.length)/100 + "%"
+		    });
+
+
+        	 }
+
+		chrome.tabs.sendMessage(sender.tab.id, {
+		    type: "coursedump_progress_upd",
+		    progress: "done"
+		});
+
+	      })()
+	}
    });
