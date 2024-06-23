aa1731cc6c630f4d9e65990430a80f7d8b2fd07d
Eltaurus-LT02 <Eltaurus@mail.ru>
2024-06-23 10:19:01 +0600

    buttons init

--- menu.js
+++ menu.js
@@ -1,7 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
+function extractNumberValue(string, key) {
+	const regex = new RegExp(`${key}(\\d+)`);
+	const match = string.match(regex);
+  
+	return match ? match[1] : "";
+}
+  
+function getDomainAndId(url) {
+	const id = extractNumberValue(url, "course_id=") ||
+			 extractNumberValue(url, "category_id=") ||
+			 extractNumberValue(url, "course/");
+	let domain = "";
+	if (url.includes("app.memrise.com")) {
+	  domain = "app.memrise.com";
+	} else if (url.includes("community-courses.memrise.com")) {
+	  domain = "community-courses.memrise.com";
+	}
+	
+	return {domain, id}
+}  
 
+async function getCurrentTabUrl() {
+  try {
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      return tab.url;
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+ 
   // links
-
   document.getElementById("link-help").addEventListener('click', function () {
     window.open('https://github.com/Eltaurus-Lt/CourseDump2022?tab=readme-ov-file#memrise-course-dump', '_blank').focus();
   });
@@ -15,6 +44,29 @@ document.addEventListener('DOMContentLoaded', function () {
     window.open('https://buymeacoffee.com/eltaurus', '_blank').focus();
   });
 
+  const tab_url = await getCurrentTabUrl();
+  const {domain, id} = getDomainAndId(tab_url);
+
+  const downloadButton = document.getElementById("download-course");
+  const BatchAddButton = document.getElementById("batch-add");
+  const BatchDownloadButton = document.getElementById("batch-download");
+  if (!domain) {
+    downloadButton.title = "Has to be used from memrise.com";
+    downloadButton.setAttribute("disabled", true);
+    BatchAddButton.title = "Needs a course page from memrise.com";
+    BatchAddButton.setAttribute("disabled", true);
+    BatchDownloadButton.title = "Has to be used from memrise.com";
+    BatchDownloadButton.setAttribute("disabled", true);
+  } else if (!id) {
+    downloadButton.title = "Needs a specific course to be opened";
+    downloadButton.setAttribute("disabled", true);
+    BatchAddButton.title = "Needs a course page from memrise.com";
+    BatchAddButton.setAttribute("disabled", true);
+  } else {
+    downloadButton.addEventListener('click', function () {
+      alert(tab_url);
+    });
+  }
 
   // document.getElementById('export').addEventListener('click', async () => {
 //     try {
